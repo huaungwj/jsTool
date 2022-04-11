430af0698a392736258723a2ef8d9d967b85e835
huaungwj <48433992+huaungwj@users.noreply.github.com>
2022-04-11 21:25:45 +0800

    2022 4-11 21:25 新增中英文排序函数
    新增中英文排序函数

--- tool.js
+++ tool.js
@@ -758,3 +758,55 @@ export const throttle = function(func, wait, type) { //函数节流 [func 函数
 	};
 };
 
+// 中英文排序
+export const pySegSort = (arr) => {
+  if (arr.length == 0) return;
+  if (!String.prototype.localeCompare) return null;
+  var letters = "#ABCDEFGHJKLMNOPQRSTWXYZ".split("");
+  var zh = "阿八嚓哒妸发旮哈讥咔垃痳拏噢妑七呥扨它穵夕丫帀".split("");
+  var segs = []; // 存放数据
+  var res = {};
+  let curr;
+  var re = /[^\u4e00-\u9fa5]/; //中文正则
+  var pattern = new RegExp("[`\\-~!@#$^&*()=|{}':;',\\[\\].<>《》/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？12345678990]"); //特殊符号
+
+  letters.filter((items, i) => {
+    curr = {
+      initial: "", //字母
+      data: [], //数据
+    };
+    arr.map((v, index) => {
+      // 特殊字符
+      if (pattern.test(v?.nickname[0])) {
+        if ((!zh[i - 1] || zh[i - 1].localeCompare(v?.nickname) <= 0) && v?.nickname.localeCompare(zh[i]) == -1) {
+          curr.data.push(v);
+        }
+      }
+      // 判断首个字是否是中文
+      if (re.test(v?.nickname[0])) {
+        // 英文
+        if (v?.nickname[0].toUpperCase() == items) {
+          curr.data.push(v);
+        }
+      } else {
+        // 中文
+        if ((!zh[i - 1] || zh[i - 1].localeCompare(v?.nickname) <= 0) && v?.nickname.localeCompare(zh[i]) == -1) {
+          curr.data.push(v);
+        }
+      }
+    });
+
+    if (curr.data.length) {
+      curr.initial = letters[i];
+      segs.push(curr);
+      curr.data.sort((a, b) => {
+        return a.nickname.localeCompare(b.nickname);
+      });
+    }
+  });
+  res.segs = Array.from(new Set(segs)); //去重
+  // console.log(res.segs);
+  const lastData = res.segs.shift();
+  res.segs.push(lastData);
+  return res;
+};
